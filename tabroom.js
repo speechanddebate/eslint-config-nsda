9b7fc2653620aa6bcf41809c66f1bd3557705422
Chris Palmer <palmer@tabroom.com>
2023-11-15 12:06:39 -0500

    Forgot about these references to react

--- tabroom.js
+++ tabroom.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "extends": ["airbnb", "./index.js", "./react.js", "./jest.js"],
+    "extends": ["airbnb", "./index.js", "./jest.js"],
     "rules": {
         "no-tabs": 0,
 		"no-use-before-define": 0,
@@ -26,17 +26,5 @@ module.exports = {
             }
           }
         ],
-        "react/jsx-indent": [
-          0,
-          "tab",
-          {
-            "checkAttributes": false,
-            "indentLogicalExpressions": true
-          }
-        ],
-        "react/jsx-indent-props": 0,
-        "react/jsx-equals-spacing": 0,
-		"react/no-unknown-property": 0,
-        "jsx-a11y/anchor-has-content": 0
     }
 };
