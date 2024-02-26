b322013a9aba7cd605e51c282489e0b43009760d
Chris Palmer <palmer@tabroom.com>
2024-02-25 20:24:58 -0500

    Sigh

--- tabroom.js
+++ tabroom.js
@@ -1,18 +1,19 @@
 module.exports = {
     "extends": ["airbnb", "./index.js", "./jest.js", "./svelte.js"],
     "rules": {
-        "no-tabs": 0,
-		"no-use-before-define": 0,
-        "key-spacing": 0,
-        "comma-spacing": 0,
-        "implicit-arrow-linebreak": 0,
-        "space-before-function-paren": 0,
-        "space-in-parens": 0,
-        "import/no-dynamic-require": 0,
-        "global-require": 0,
-        "padded-blocks": 0,
-		"no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
-        "no-plusplus": 0,
+        "no-tabs"                     : 0,
+		"no-use-before-define"        : 0,
+        "key-spacing"                 : 0,
+        "comma-spacing"               : 0,
+        "implicit-arrow-linebreak"    : 0,
+        "space-before-function-paren" : 0,
+		"no-await-in-loop"            : 0,
+        "space-in-parens"             : 0,
+        "import/no-dynamic-require"   : 0,
+        "global-require"              : 0,
+        "padded-blocks"               : 0,
+		"no-restricted-syntax"        : ["error", "LabeledStatement", "WithStatement"],
+        "no-plusplus"                 : 0,
         "indent": [
           "error",
           "tab"
@@ -21,8 +22,8 @@ module.exports = {
           0,
           {
             "exceptions": {
-              "Property": true,
-              "VariableDeclarator": true
+              "Property"           : true,
+              "VariableDeclarator" : true
             }
           }
         ],
